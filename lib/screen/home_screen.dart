4133507a1f2b0f4b053be9ddfaa2d5feaf1fc00e
pazuru1216 <pazuru1216@gmail.com>
2025-03-16 12:51:40 +0900

    add header basic

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,24 +6,89 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFE0E0E0),
-      body:Column(
-        children: [
-          ///프사, 알림, +버튼
-          Row(
-            children: [],
+        backgroundColor: Color(0xFFE0E0E0),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Container(
+              child: Column(
+                children: [
+                  ///첫번째 줄
+                  Container(
+                    color: Colors.red,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20)
+                            )
+                          ),
+                        ),
+
+                        ///알림, 추가버튼
+                        Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(20)
+                                    )
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(20)
+                                    )
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  /// 성취 알람칸들
+                  Container(
+                    child: Row(
+                      children: [],
+                    ),
+                  ),
+
+                  /// Recent Documents
+                  /// Title
+                  Container(
+                    child: Row(
+                      children: [],
+                    ),
+                  ),
+
+                  /// Cards
+                  Container(
+                    child: Column(
+                      children: [],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          /// 성취 알람칸들
-          Row(children: [],),
-          /// Recent Documents
-          /// Title
-          Row(children: [],),
-          /// Cards
-          Column(
-            children: [],
-          ),
-        ],
-      )
-    );
+        ));
   }
 }
