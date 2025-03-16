7b3479b4c71384dc8d6e5bfb9fcf08ce673abb69
pazuru1216 <pazuru1216@gmail.com>
2025-03-16 11:42:45 +0900

    cloning desing basic setting

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:design_clone_1/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE0E0E0),
+      body:Column(
+        children: [
+          ///프사, 알림, +버튼
+          Row(
+            children: [],
+          ),
+          /// 성취 알람칸들
+          Row(children: [],),
+          /// Recent Documents
+          /// Title
+          Row(children: [],),
+          /// Cards
+          Column(
+            children: [],
+          ),
+        ],
+      )
+    );
+  }
+}
